bd48fbc3ad7276f5d617f1cf405685b2fd84bc02
Ben Southgate <bsouthga@gmail.com>
2017-04-13 14:59:40 -0400

    chore: change name, deps

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,28 @@
-import * as express from 'express';
+import { Tyr as Tyranid } from 'tyranid';
 
-const app = express();
 
+export default generate;
+
+
+export interface TyranidSwaggerOptions {
+  /**
+   * api version, defaults to 1
+   */
+  version: number;
+}
+
+
+
+/**
+ * Given an instance of tyranid, create a swagger api spec
+ *
+ * @param Tyr initialized tyranid object
+ * @param options schema generation options
+ */
+async function generate(
+  Tyr: typeof Tyranid,
+  options?: TyranidSwaggerOptions
+) {
+
+  return '';
+}
