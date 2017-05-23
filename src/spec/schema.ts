dbc73fca3980b66d59895ae73af2b24eb8fc0454
Ben Southgate <bsouthga@gmail.com>
2017-05-23 10:47:57 -0400

    fix: check for openAPI property before adding to required list

--- src/spec/schema.ts
+++ src/spec/schema.ts
@@ -333,7 +333,7 @@ function getRequiredChildProps(field: Tyr.FieldInstance | Tyr.CollectionDefiniti
   each(field.fields, (f, name) => {
     const opts = options(f.def);
     const propName = opts.name || name;
-    if (f.def.required) props.push(propName);
+    if (f.def.openAPI && f.def.required) props.push(propName);
   });
 
   return props;
